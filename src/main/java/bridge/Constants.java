1882fabaf89a37aec15cd3f300b309aa415db31c
LEE-sh1673 <lsh901673@gmail.com>
2022-11-20 19:16:55 +0900

    refactor(view): 입력을 받는 view에 쓰이는 리터럴 문자열 값을 상수로 추가

--- src/main/java/bridge/Constants.java
+++ src/main/java/bridge/Constants.java
@@ -13,4 +13,10 @@ public final class Constants {
     public static final String BRIDGE_TAIL = " ]";
 
     public static final String BRIDGE_DELIMITER = " | ";
+
+    public static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+
+    public static final String INPUT_MOVING_SQUARE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+
+    public static final String INPUT_RESTART_OR_TERMINATE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 }
