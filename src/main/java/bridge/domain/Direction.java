d7acd479bd353282a3530d64e8752feb6abf0bcd
LEE-sh1673 <lsh901673@gmail.com>
2022-11-20 16:07:13 +0900

    refactor(bridge): 방향을 나타내는 Direction 클래스에 다리 생성을 위한 필드 추가

--- src/main/java/bridge/domain/Direction.java
+++ src/main/java/bridge/domain/Direction.java
@@ -5,13 +5,16 @@ import static bridge.domain.ErrorMessage.INVALID_DIRECTION;
 import java.util.Arrays;
 
 public enum Direction {
-    UP("U"),
-    DOWN("D");
+    UP("U", 1),
+    DOWN("D", 0);
 
     private final String mark;
 
-    Direction(final String mark) {
+    private final int number;
+
+    Direction(final String mark, final int number) {
         this.mark = mark;
+        this.number = number;
     }
 
     public static Direction of(final String mark) {
@@ -23,7 +26,24 @@ public enum Direction {
             );
     }
 
+    public static Direction of(final int number) {
+        return Arrays.stream(values())
+            .filter(direction -> direction.matchNumber(number))
+            .findAny()
+            .orElseThrow(() ->
+                new IllegalArgumentException(INVALID_DIRECTION.getMessage())
+            );
+    }
+
+    private boolean matchNumber(final int number) {
+        return this.number == number;
+    }
+
     private boolean matchMark(final String mark) {
         return this.mark.equals(mark);
+    }
+
+    public String getMark() {
+        return mark;
     }
 }
