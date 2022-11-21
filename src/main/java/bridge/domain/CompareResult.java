d6829bb43469283cdefa010068a3375c47f1d7a7
LEE-sh1673 <lsh901673@gmail.com>
2022-11-21 21:54:39 +0900

    refactor(bridge): CompareResult 클래스 구조 변경
    - 도메인 관련 데이터에 view에서 출력할 문자들이 있는 것은 적절하지 않다고 판단하여 구조를 변경함

--- src/main/java/bridge/domain/CompareResult.java
+++ src/main/java/bridge/domain/CompareResult.java
@@ -1,17 +1,5 @@
 package bridge.domain;
 
 public enum CompareResult {
-    MATCH("O"),
-    MISS("X"),
-    NONE(" ");
-
-    private final String mark;
-
-    CompareResult(final String mark) {
-        this.mark = mark;
-    }
-
-    public String getMark() {
-        return mark;
-    }
+    MATCH, MISS, NONE
 }
