6e8b0d857d65f3022fb16d6fc0761995c4ac3c3c
LEE-sh1673 <lsh901673@gmail.com>
2022-11-24 21:42:52 +0900

    feat(bridge): 다리의 칸과 특정 칸을 비교하는 기능(MATCH)추가

--- src/test/java/bridge/model/BridgeTest.java
+++ src/test/java/bridge/model/BridgeTest.java
@@ -8,13 +8,20 @@ import org.junit.jupiter.api.Test;
 
 public class BridgeTest {
 
+    static Bridge bridge = new Bridge(List.of("U", "D", "D"));
+
     @DisplayName("다리의 칸과 특정 칸을 비교할 수 있다. - MISS")
     @Test
     void compareTileWithBridgeMISS() {
-        Bridge bridge = new Bridge(List.of("U", "D", "D"));
         Tile tile = new Tile("U", 2);
-        CompareResult compareResult = bridge.contains(tile);
-        assertThat(compareResult).isEqualTo(CompareResult.MISS);
+        assertThat(bridge.contains(tile)).isEqualTo(CompareResult.MISS);
+    }
+
+    @DisplayName("다리의 칸과 특정 칸을 비교할 수 있다. - MATCH")
+    @Test
+    void compareTileWithBridgeMATCH() {
+        Tile tile = new Tile("U", 1);
+        assertThat(bridge.contains(tile)).isEqualTo(CompareResult.MATCH);
     }
 
 }
